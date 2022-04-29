7e916c1e7267030b4d5e8710a6fd2b00d5c7e969
Markus Engelund Mathiasen <202005875@post.au.dk>
2022-04-29 11:34:40 +0200

    Add ugly point count

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,31 @@ import GameArena from './GameArena.vue'
 export default {
   components: {
     GameArena
+  },
+  data() {
+    return {
+      redPoints: 0,
+      bluePoints: 0
+    }
+  },
+  methods: {
+    setupArena() {
+      this.$refs.arena.setup();
+    },
+    redDead() {
+      this.redPoints++;
+      this.setupArena();
+    },
+    blueDead() {
+      this.bluePoints++;
+      this.setupArena();
+    }
   }
 }
 </script>
 
 <template>
-  <GameArena ref="arena" :width="1700" :height="700" @redDead="$refs.arena.setup()" @blueDead="$refs.arena.setup()" />
+  <GameArena ref="arena" :width="1700" :height="700" @redDead="redDead()" @blueDead="blueDead()" />
+  <h3>Blue points: {{bluePoints}}</h3>
+  <h3>Red points: {{redPoints}}</h3>
 </template>
