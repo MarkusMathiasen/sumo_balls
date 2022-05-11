bd303aa5efa171314a9b77bafe2bd2929bf21371
Markus Engelund Mathiasen <202005875@post.au.dk>
2022-05-11 21:45:43 +0200

    Finish tests for ball

--- test/ball.test.js
+++ test/ball.test.js
@@ -1,11 +1,12 @@
-import { beforeEach, describe, expect, test, vi } from 'vitest'
+import { describe, expect, test, vi } from 'vitest'
+import { fireEvent } from '@testing-library/vue'
 import { useBall } from '@/composables/ball'
 
 const ctx = {beginPath: vi.fn(), arc: vi.fn(), clearRect: vi.fn(), fill: vi.fn() };
 const defaultRadius = 50;
 const defaultColor = 'red';
-const defaultAcceleration = 0.3;
-const defaultKeys = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'];
+const defaultAcceleration = 1;
+const defaultKeys = {up: 'ArrowUp', down: 'ArrowDown', left: 'ArrowLeft', right: 'ArrowRight'};
 
 function createBall(params) {
   return useBall(ctx,
@@ -64,4 +65,84 @@ test.each([
   expect(sy).toBeLessThanOrEqual(y);
   expect(sx+dx).toBeGreaterThanOrEqual(x);
   expect(sy+dy).toBeGreaterThanOrEqual(y);
+});
+
+describe('Movement', () => {
+  test.each([0.3, 0.5, 1.1])('Movement with acceleration %d', (accel) => {
+    let ball = createBall({acceleration: accel});
+    expect(ball.x).toBeCloseTo(0);
+    expect(ball.y).toBeCloseTo(0);
+    expect(ball.speed_x).toBeCloseTo(0);
+    expect(ball.speed_y).toBeCloseTo(0);
+    fireEvent.keyDown(window, {
+      key: defaultKeys.right,
+      code: defaultKeys.right
+    });
+    ball.update();
+    expect(ball.speed_x).toBeCloseTo(accel);
+    expect(ball.speed_y).toBeCloseTo(0);
+    expect(ball.x).toBeCloseTo(accel);
+    expect(ball.y).toBeCloseTo(0);
+    ball.update();
+    expect(ball.speed_x).toBeCloseTo(2*accel);
+    expect(ball.speed_y).toBeCloseTo(0);
+    expect(ball.x).toBeCloseTo(3*accel);
+    expect(ball.y).toBeCloseTo(0);
+    fireEvent.keyUp(window, {
+      key: defaultKeys.right,
+      code: defaultKeys.right
+    });
+  });
+
+  test.each([
+    ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'],
+    ['w', 's', 'a', 'd'],
+    ['8', '5', '4', '6']
+  ])('Test different keys for movement: %s %s %s %s', (up, down, left, right) => {
+    let ball = createBall({keys: {up, down, left, right}});
+    fireEvent.keyDown(window, {
+      key: up,
+      code: up
+    });
+    ball.update();
+    expect(ball.x).toBeCloseTo(0);
+    expect(ball.y).toBeCloseTo(-defaultAcceleration);
+    fireEvent.keyUp(window, {
+      key: up,
+      code: up
+    });
+    fireEvent.keyDown(window, {
+      key: right,
+      code: right
+    });
+    ball.update();
+    expect(ball.x).toBeCloseTo(defaultAcceleration);
+    expect(ball.y).toBeCloseTo(-2*defaultAcceleration);
+    fireEvent.keyUp(window, {
+      key: right,
+      code: right
+    });
+    fireEvent.keyDown(window, {
+      key: down,
+      code: down
+    });
+    ball.update();
+    expect(ball.x).toBeCloseTo(2*defaultAcceleration);
+    expect(ball.y).toBeCloseTo(-2*defaultAcceleration);
+    fireEvent.keyUp(window, {
+      key: down,
+      code: down
+    });
+    fireEvent.keyDown(window, {
+      key: left,
+      code: left
+    });
+    ball.update();
+    expect(ball.x).toBeCloseTo(2*defaultAcceleration);
+    expect(ball.y).toBeCloseTo(-2*defaultAcceleration);
+    fireEvent.keyUp(window, {
+      key: left,
+      code: left
+    });
+  });
 });
